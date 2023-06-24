612b617f42d7c8bead8ad549cf573b2e6a94a545
Maksim <bobreczovm@inbox.ru>
2023-06-25 00:48:42 +0300

    Функция сохранят в базу данных Postgres и отдовать список  из  базы данных Postgres

--- Connection_Database_Postgres.cs
+++ Connection_Database_Postgres.cs
@@ -1,17 +1,30 @@
 ﻿
 using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.Logging;
 using Npgsql;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace Api_Apathe
 {
     public class Connection_Database_Postgres
     {
-        
+        /// <summary>
+        /// Создает подключение  доступное только в этом классе
+        /// </summary>
         public string connectionString = "Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=1";
 
+        /// <summary>
+        /// Создает подключение статическое
+        /// </summary>
         public static string conectionString = "Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=1";
 
+
+        /// <summary>
+        /// Создает таблицу  пользователь 
+        /// </summary>
         public void Create_Table_User(Connection_Database_Postgres _Postgres)
         {
             using (var connection = new NpgsqlConnection(_Postgres.connectionString))
@@ -44,6 +57,9 @@ namespace Api_Apathe
         }
 
 
+        /// <summary>
+        /// Проверяет пользователя 
+        /// </summary>
         public bool Check_user(bool Users, Connection_Database_Postgres _Postgres, string Name, string Password)
         {
 
@@ -73,10 +89,15 @@ namespace Api_Apathe
             return Users;
         }
 
+
+        /// <summary>
+        ///Добавляет пользователя
+        /// </summary>
         public bool Insert_user(bool Users, Connection_Database_Postgres _Postgres, string Name, string Password)
         {
 
             string Sql_Select = $"INSERT INTO Users(Name, Password) VALUES ('{Name}', '{Password}');";
+
             using (var connection = new NpgsqlConnection(_Postgres.connectionString))
             {
                 connection.Open();
@@ -88,5 +109,227 @@ namespace Api_Apathe
             return Users;
 
         }
-    }
+
+
+        /// <summary>
+        /// Добавляет logi 
+        /// </summary>
+        public void Insert_postgres(Connection_Database_Postgres _Postgres)
+        {
+            var path = Environment.CurrentDirectory.ToString();
+
+            string pattern = @"^(.*?) - - \[(.*?)\] ""(.*?)"" (\d+) (\d+)$";
+
+            // Создание регулярного выражения
+            var regex = new Regex(pattern);
+
+            using (var reader = new StreamReader(path + "\\access_log"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Поиск совпадений в строке лог-файла
+                    var match = regex.Match(line);
+
+                    // Обработка совпадений
+                    if (match.Success)
+                    {
+                        var ip = match.Groups[1].Value;
+                        var datetime = match.Groups[2].Value;
+                        var request = match.Groups[3].Value;
+                        var status = match.Groups[4].Value;
+                        var size = match.Groups[5].Value;
+
+                        // var sises = match.Groups[6].Value;
+                        Console.WriteLine($"IP: {ip} Request: {request}");
+
+                    
+                        //     string dat = "28/Jul/2006:10:27:10 -0300";
+                        string format = "dd/MMM/yyyy:HH:mm:ss zzz";
+
+                        DateTime datetimes = DateTime.ParseExact(datetime, format, CultureInfo.InvariantCulture);
+                        int CountLog = 0;
+                        bool id_logs = false;
+                        using (var connection = new NpgsqlConnection(_Postgres.connectionString))
+                        {
+                            connection.Open();
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                
+                                var SqL = "SELECT Count(*) AS rec_count FROM logs";
+                                //command.ExecuteNonQuery();
+                                NpgsqlCommand npgsqlCommand2 = new NpgsqlCommand(SqL, connection);
+                                npgsqlCommand2.ExecuteNonQuery();
+
+
+                                NpgsqlDataReader npgsqlDataReader = npgsqlCommand2.ExecuteReader();
+
+                                //  NpgsqlDataReader npgsqlDataReade = npgsqlCommand3.ExecuteReader();
+                                SqL = "";
+                                if (npgsqlDataReader.HasRows == true)
+                                {
+                                    while (npgsqlDataReader.Read())
+                                    {
+                                        CountLog = Convert.ToInt32(npgsqlDataReader["rec_count"]);
+                                    }
+                                    if (CountLog == 12)
+                                    {
+                                        id_logs = true;
+                                    }
+                                    else
+                                    {
+
+                                    }
+                                }
+                                else
+                                {
+                                    id_logs = false;
+                                }      
+                            }
+                        }
+                        if (id_logs == true)
+                        {
+
+                        }
+                        else
+                        {
+                            using (var connections = new NpgsqlConnection(_Postgres.connectionString))
+                            {
+                                connections.Open();
+
+                                // Вставка логов в таблицу
+                                string Insert_logs = $"INSERT INTO logs (ip, date, request, status, size) VALUES ('{ip}','{datetimes:G}', '{request}','{status}', {size})";
+
+                                NpgsqlCommand npgsqlCommand = new NpgsqlCommand(Insert_logs, connections);
+                                npgsqlCommand.ExecuteNonQuery();
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Выводит  список логов в Json[]
+        /// </summary>
+        public Json[] Select_logi(Connection_Database_Postgres _Postgres)
+        {
+            Json[] logs;
+            int CountLog = 0;
+            using (var connection = new NpgsqlConnection(_Postgres.connectionString))
+            {
+                connection.Open();
+                using (MemoryStream ms = new MemoryStream())
+                {
+
+                    var SqL = "SELECT Count(*) AS rec_count FROM logs";
+                    //command.ExecuteNonQuery();
+                    NpgsqlCommand npgsqlCommand2 = new NpgsqlCommand(SqL, connection);
+                    npgsqlCommand2.ExecuteNonQuery();
+
+
+                    NpgsqlDataReader npgsqlDataReader = npgsqlCommand2.ExecuteReader();
+
+                    //  NpgsqlDataReader npgsqlDataReade = npgsqlCommand3.ExecuteReader();
+                    SqL = "";
+                    if (npgsqlDataReader.HasRows == true)
+                    {
+                        while (npgsqlDataReader.Read())
+                        {
+                            CountLog = Convert.ToInt32(npgsqlDataReader["rec_count"]);
+                        }
+                      
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+
+            Json[] log = new Json[CountLog];
+            using (var connection = new NpgsqlConnection(_Postgres.connectionString))
+            {
+                connection.Open();
+                using (MemoryStream ms = new MemoryStream())
+                {
+
+                    var SqL = "SELECT id_logs, ip, date, request, status, size FROM public.logs;";
+                    //command.ExecuteNonQuery();
+                    NpgsqlCommand npgsqlCommand2 = new NpgsqlCommand(SqL, connection);
+                    npgsqlCommand2.ExecuteNonQuery();
+
+
+                    NpgsqlDataReader npgsqlDataReader = npgsqlCommand2.ExecuteReader();
+
+                    //  NpgsqlDataReader npgsqlDataReade = npgsqlCommand3.ExecuteReader();
+                    SqL = "";
+                    int i = 0;
+                    if (npgsqlDataReader.HasRows == true)
+                    {
+                        while (npgsqlDataReader.Read())
+                        {
+                            Json Log = new Json(Convert.ToString(  npgsqlDataReader["Ip"]),
+                             Convert.ToDateTime(   npgsqlDataReader["date"]),
+                              Convert.ToString(npgsqlDataReader["request"]),
+                              Convert.ToString(npgsqlDataReader["status"]),
+                               Convert.ToString(npgsqlDataReader["size"]));
+                            log[i] = Log;
+                            i++;
+                        }
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+
+            logs = log;
+
+            return logs;
+        }
+            ///  
+            //using (FileStream json_Fille = new FileStream(path + "\\data.json", FileMode.Append))
+            //{
+            //    if (json_Fille.Length == 0)
+            //    {
+            //        Byte[] Aa = Encoding.Default.GetBytes("[");
+            //        json_Fille.Write(Aa, 0, Aa.Length);
+            //    }
+            //    var options = new JsonSerializerOptions
+            //    {
+            //        AllowTrailingCommas = true
+            //    };
+
+            //    Json json = new Json(ip, datetimes, request, status, size);
+            //    JsonSerializer.Serialize<Json>(json_Fille, json, options);
+            //    if (CountLog < 11)
+            //    {
+            //        Byte[] Aa2 = Encoding.Default.GetBytes(",");
+            //        json_Fille.Write(Aa2, 0, Aa2.Length);
+            //    }
+            //    if (CountLog == 11)
+            //    {
+            //        Byte[] Aa3 = Encoding.Default.GetBytes("]");
+            //        json_Fille.Write(Aa3, 0, Aa3.Length);
+            //    }
+            //}
+            //long endPoint = json_Fille.Length;
+            // Set the stream position to the end of the file.        
+            // json_Fille.Seek(endPoint, SeekOrigin.Begin);
+
+            //Что-то падает
+            //    npgsqlCommand.CommandText = Insert_logs;
+            //Count = 0;
+
+
+            /////////////////
+
+
+
+        }
 }
--- SelectionForm.cs
+++ SelectionForm.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 using Microsoft.VisualBasic.ApplicationServices;
 using static Npgsql.PostgresTypes.PostgresCompositeType;
 using System.Text;
-using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json.Linq;
 
 namespace Api_Apathe
 {
@@ -39,228 +39,100 @@ namespace Api_Apathe
         {
             try
             {
-                var path = Environment.CurrentDirectory.ToString();
-                string pattern = @"^(.*?) - - \[(.*?)\] ""(.*?)"" (\d+) (\d+)$";
+                Postgres.Insert_postgres(Postgres);
 
-                // Создание регулярного выражения
-                var regex = new Regex(pattern);
-                using (var reader = new StreamReader(path + "\\access_log"))
+                 var data  =  Postgres.Select_logi(Postgres);
+                for (int i = 1; i < data.Length; i++)
                 {
+                    DataGridViewTextBoxCell Ip = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[0];
+                    Ip.Value = data[i].Ip;
 
+                    DataGridViewTextBoxCell Date = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[1];
+                    Date.Value = data[i].Date;
 
+                    DataGridViewTextBoxCell Request = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[2];
+                    Request.Value = data[i].Request;
+                    DataGridViewTextBoxCell Status = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[3];
+                    Status.Value = data[i].Status;
 
-
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        // Поиск совпадений в строке лог-файла
-                        var match = regex.Match(line);
-
-                        // Обработка совпадений
-                        if (match.Success)
-                        {
-                            var ip = match.Groups[1].Value;
-                            var datetime = match.Groups[2].Value;
-                            var request = match.Groups[3].Value;
-                            var status = match.Groups[4].Value;
-                            var size = match.Groups[5].Value;
-
-                            // var sises = match.Groups[6].Value;
-                            Console.WriteLine($"IP: {ip} Request: {request}");
-
-                            // Создание базы данных
-                            //     string dat = "28/Jul/2006:10:27:10 -0300";
-                            string format = "dd/MMM/yyyy:HH:mm:ss zzz";
-
-                            DateTime datetimes = DateTime.ParseExact(datetime, format, CultureInfo.InvariantCulture);
-                            int CountLog = 0;
-                            bool id_logs = false;
-                            using (var connection = new NpgsqlConnection(Connection_Database_Postgres.conectionString))
-                            {
-                                connection.Open();
-                                using (MemoryStream ms = new MemoryStream())
-                                {
-                                    //using (FileStream json_Fille = new FileStream("data.json", FileMode.OpenOrCreate))
-                                    //{
-                                    var SqL = "SELECT Count(*) AS rec_count FROM logs";
-                                    //command.ExecuteNonQuery();
-                                    NpgsqlCommand npgsqlCommand2 = new NpgsqlCommand(SqL, connection);
-                                    npgsqlCommand2.ExecuteNonQuery();
-                                    //NpgsqlCommand npgsqlCommand = new NpgsqlCommand(SqL, connection);
-                                    //npgsqlCommand.ExecuteNonQuery();
-                                    ///NpgsqlCommand npgsqlCommand3 = new NpgsqlCommand(SqL, connection);
-                                    //npgsqlCommand3.ExecuteNonQuery();
-
-                                    NpgsqlDataReader npgsqlDataReader = npgsqlCommand2.ExecuteReader();
-
-                                    //  NpgsqlDataReader npgsqlDataReade = npgsqlCommand3.ExecuteReader();
-                                    SqL = "";
-                                    if (npgsqlDataReader.HasRows == true)
-                                    {
-                                        while (npgsqlDataReader.Read())
-                                        {
-                                            CountLog = Convert.ToInt32(npgsqlDataReader["rec_count"]);
-                                        }
-                                        if (CountLog == 12)
-                                        {
-                                            id_logs = true;
-                                        }
-                                        else
-                                        {
-
-                                        }
-                                    }
-                                    else
-                                    {
-                                        id_logs = false;
-                                    }
-                                    //}
-                                }
-                            }
-
-
-                            // Что -то пошло не так
-                            if (id_logs == true)
-                            {
-
-                            }
-                            else
-                            {
-                                using (FileStream json_Fille = new FileStream(path + "\\data.json", FileMode.Append))
-                                {
-                                    using (var connections = new NpgsqlConnection(Connection_Database_Postgres.conectionString))
-                                    {
-                                        connections.Open();
-                                        var options = new JsonSerializerOptions
-                                        {
-                                            AllowTrailingCommas = true
-                                        };
-
-                                        if (json_Fille.Length == 0)
-                                        {
-                                            Byte[] Aa = Encoding.Default.GetBytes("[");
-                                            json_Fille.Write(Aa, 0, Aa.Length);
-                                        }
-
-                                        //long endPoint = json_Fille.Length;
-                                        // Set the stream position to the end of the file.        
-                                        // json_Fille.Seek(endPoint, SeekOrigin.Begin);
-
-                                        Json json = new Json(ip, datetimes, request, status, size);
-                                        JsonSerializer.Serialize<Json>(json_Fille, json, options);
-                                        if (CountLog < 11)
-                                        {
-                                            Byte[] Aa2 = Encoding.Default.GetBytes(",");
-                                            json_Fille.Write(Aa2, 0, Aa2.Length);
-                                        }
-                                        // Вставка логов в таблицу
-                                        string Insert_logs = $"INSERT INTO logs (ip, date, request, status, size) VALUES ('{ip}','{datetimes:G}', '{request}','{status}', {size})";
-
-                                        NpgsqlCommand npgsqlCommand = new NpgsqlCommand(Insert_logs, connections);
-
-                                        //Что-то падает
-                                        //    npgsqlCommand.CommandText = Insert_logs;
-                                        npgsqlCommand.ExecuteNonQuery();
-                                    }
-                                    if (CountLog == 11)
-                                    {
-                                        Byte[] Aa3 = Encoding.Default.GetBytes("]");
-                                        json_Fille.Write(Aa3, 0, Aa3.Length);
-                                    }
-                                    //Count = 0;
-                                }
-                            }
-                        }
-
-                    }
-
-                    int CountLogs = 0;
-
-                    using (var connection = new NpgsqlConnection(Connection_Database_Postgres.conectionString))
-                    {
-                        connection.Open();
-
-                        var SqL = "SELECT Count(*) AS rec_count FROM logs";
-
-                        NpgsqlCommand npgsqlCommand2 = new NpgsqlCommand(SqL, connection);
-                        npgsqlCommand2.ExecuteNonQuery();
-                        //NpgsqlCommand npgsqlCommand = new NpgsqlCommand(SqL, connection);
-                        //npgsqlCommand.ExecuteNonQuery();
-                        ///NpgsqlCommand npgsqlCommand3 = new NpgsqlCommand(SqL, connection);
-                        //npgsqlCommand3.ExecuteNonQuery();
-
-                        NpgsqlDataReader npgsqlDataReader = npgsqlCommand2.ExecuteReader();
-
-                        SqL = "";
-                        if (npgsqlDataReader.HasRows == true)
-                        {
-                            while (npgsqlDataReader.Read())
-                            {
-                                CountLogs = Convert.ToInt32(npgsqlDataReader["rec_count"]);
-                            }
-
-                        }
-                        else
-                        {
-
-                        }
-                    }
-
-                    using (FileStream json_Fille = new FileStream(path + "\\data.json", FileMode.OpenOrCreate))
-                    {
-                        //    int count = 0;
-                        //    byte[] buffer = new byte[1024];
-                        //   count =  json_Fille.Read(buffer, 0, buffer.Length);
-
-
-
-                        //     Json_T log = JsonSerializer.Deserialize<Json_T>(json_Fille);
-                        JObject log = JObject.Parse(json_Fille);
-                        if (log == null)
-                        {
-
-                        }
-                        else
-                        {
-                          
-
-                            Json[] jsons = new Json[CountLogs];
-                            //for (int i = 0; i < jsons.Length; i++)
-                            //{
-                            //    jsons[i] = log;
-                            //}
-                            // jsons[Count] = log;
-
-                          //  logs = log;
-                            jsons = null;
-                        }
-
-                    }
-
-                    //Колонку по умолчанию сдесь упало
-                    for (int i = 1; i < logs.Length; i++)
-                    {
-                        DataGridViewTextBoxCell Ip = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[0];
-                        Ip.Value = logs[i].Ip;
-
-                        DataGridViewTextBoxCell Date = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[1];
-                        Date.Value = logs[i].Date;
-
-                        DataGridViewTextBoxCell Request = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[2];
-                        Request.Value = logs[i].Request;
-                        DataGridViewTextBoxCell Status = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[3];
-                        Status.Value = logs[i].Status;
-
-                        DataGridViewTextBoxCell Size = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[4];
-                        Size.Value = logs[i].Size;
-                    }
-
-                    //Добоавляем колонку друзей
-                    //DataGridViewImageColumn imgColumn = new DataGridViewImageColumn();
-                    ////Называем колонку для картинок
-                    //imgColumn.Name = "Images";
+                    DataGridViewTextBoxCell Size = (DataGridViewTextBoxCell)dataGridView1.Rows[i].Cells[4];
+                    Size.Value = data[i].Size;
                 }
+
+
+
+                //int CountLogs = 0;
+
+                //using (var connection = new NpgsqlConnection(Connection_Database_Postgres.conectionString))
+                //{
+                //    connection.Open();
+
+                //    var SqL = "SELECT Count(*) AS rec_count FROM logs";
+
+                //    NpgsqlCommand npgsqlCommand2 = new NpgsqlCommand(SqL, connection);
+                //    npgsqlCommand2.ExecuteNonQuery();
+                //    //NpgsqlCommand npgsqlCommand = new NpgsqlCommand(SqL, connection);
+                //    //npgsqlCommand.ExecuteNonQuery();
+                //    ///NpgsqlCommand npgsqlCommand3 = new NpgsqlCommand(SqL, connection);
+                //    //npgsqlCommand3.ExecuteNonQuery();
+
+                //    NpgsqlDataReader npgsqlDataReader = npgsqlCommand2.ExecuteReader();
+
+                //    SqL = "";
+                //    if (npgsqlDataReader.HasRows == true)
+                //    {
+                //        while (npgsqlDataReader.Read())
+                //        {
+                //            CountLogs = Convert.ToInt32(npgsqlDataReader["rec_count"]);
+                //        }
+
+                //    }
+                //    else
+                //    {
+
+                //    }
+                //}
+
+                //using (FileStream json_Fille = new FileStream(path + "\\data.json", FileMode.OpenOrCreate))
+                //{
+                //    int count = 0;
+                //    byte[] buffer = new byte[1024];
+                //   count =  json_Fille.Read(buffer, 0, buffer.Length);
+
+
+
+                //     Json_T log = JsonSerializer.Deserialize<Json_T>(json_Fille);
+                //  JObject log = JObject.Parse(json_Fille);
+                //    if (log == null)
+                //    {
+
+                //    }
+                //    else
+                //    {
+
+
+                //        Json[] jsons = new Json[CountLogs];
+                //        //for (int i = 0; i < jsons.Length; i++)
+                //        //{
+                //        //    jsons[i] = log;
+                //        //}
+                //        // jsons[Count] = log;
+
+                //      //  logs = log;
+                //        jsons = null;
+                //    }
+
+                //}
+
+                //Колонку по умолчанию сдесь упало
+
+
+                //Добоавляем колонку друзей
+                //DataGridViewImageColumn imgColumn = new DataGridViewImageColumn();
+                ////Называем колонку для картинок
+                //imgColumn.Name = "Images";
             }
+            
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
